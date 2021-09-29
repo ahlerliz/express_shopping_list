cd3216ce3eff9430d6a587718e8836da7f9b87cb
davisrao <davisrao1@gmail.com>
2021-09-29 16:49:37 -0700

    made 3 routes: delete, patch, and get for 1 single item

--- itemRoutes.js
+++ itemRoutes.js
@@ -15,7 +15,7 @@ router.get("/", function (req, res, next) {
   return res.json({items});
 });
 
-/** DELETE /users/[id]: delete user, return {message: Deleted} */
+/** POST /items/ add new item and return {added: {name:name, price:price}} */
 router.post("/", function (req, res, next) {
   let name = req.body.name;
   let price = req.body.price;
@@ -24,5 +24,54 @@ router.post("/", function (req, res, next) {
 
   return res.json({ added: {name, price} });
 });
+
+/** GET single item from /items/:name return {{name:name, price:price}} */
+router.get("/:name", function (req, res, next) {  
+  let name = req.params.name;
+
+  let singleItem = items.filter(n => n.name === name)
+  let item = singleItem[0]
+
+  return res.json(item);
+});
+
+/** PATCH /items/name edit item and edited {added: {name:name, price:price}} */
+router.patch("/:name", function (req, res, next) {
+  let oldName = req.params.name;
+  // let price = req.body.price;
+
+  let singleItem = items.filter(n => n.name === oldName)
+  let item = singleItem[0]
+
+  let itemIndex = items.indexOf(item)
+
+  if(req.body.name){
+    item.name =req.body.name
+  }else if(req.body.price){
+    item.price =req.body.price
+  }
+
+  items[itemIndex] = item
+
+  return res.json({ updated: {item} });
+});
+
+/** DELETE /items/name delete item and return {message:deleted} */
+router.delete("/:name", function (req, res, next) {
+  let name = req.params.name;
+
+  let singleItem = items.filter(n => n.name === name)
+
+  let item = singleItem[0]
+
+  let itemIndex = items.indexOf(item)
+
+  console.log("this is item index",itemIndex)
+
+  items.splice(itemIndex,1)
+
+  return res.json({ message: "deleted" });
+});
+
 
 module.exports = router;
